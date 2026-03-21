b9721e7bc5e974bd85a2350a544f6f99828c2f3f
kamogotjhb024 <kamogotjhb024@student.wethinkcode.co.za>
2026-03-21 17:57:48 +0200

    basic home page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/admin', function () {
+    return view('Admin page coming soon');
+});
+
+Route::get('/user', function () {
     return view('welcome');
 });
