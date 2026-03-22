27bc7084c5238fccd610f3e24d898be03e2425c4
Kagiso Mogotsi <85134032+blaQPablo88@users.noreply.github.com>
2026-03-22 11:42:53 +0200

    Update Booking.php
    Docstrings on Booking.php

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,9 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Booking extends Model
-{
+/**
+ * Class Booking
+ *
+ * Represents a user booking for a specific employee timeslot.
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $user_email
+ * @property int $employee_timeslot_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @property-read \App\Models\EmployeeTimeslot $employeeTimeslot
+ */
+
+class Booking extends Model {
     use HasFactory;
+
+/**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
 
     protected $fillable = [
         'user_email',
