756eb2e832bc639119ba1571b8ce2f1b0f220616
Kagiso Mogotsi <85134032+blaQPablo88@users.noreply.github.com>
2026-03-22 11:45:34 +0200

    Update Timeslot.php
    Docstrings on Timeslot.php

--- app/Models/Timeslot.php
+++ app/Models/Timeslot.php
@@ -5,9 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class Timeslot
+ *
+ * Represents a time window that can be assigned to one or more employees.
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $start_time
+ * @property string $end_time
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\EmployeeTimeslot[] $employeeTimeslots
+ */
+
 class Timeslot extends Model
 {
     use HasFactory;
+
+/**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
 
     // Allows mass assignment when creating timeslots
     protected $fillable = [
