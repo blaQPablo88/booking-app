fdf91f23f4e0a6eae25ebcf9f0d28d29134224d4
Kagiso Mogotsi <85134032+blaQPablo88@users.noreply.github.com>
2026-03-22 11:44:14 +0200

    Update Employee.php
    Docstrings on Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,9 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class Employee
+ *
+ * Represents an employee that can be assigned to timeslots.
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $full_name
+ * @property string $position
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\EmployeeTimeslot[] $employeeTimeslots
+ */
+
 class Employee extends Model
 {
     use HasFactory;
+
+/**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
 
     protected $fillable = [
         'full_name', 
